78a9b226780e5f0f0c047fa50b7357e175b4175b
Curtis-Stilwell <stilcurt@yahoo.com>
2019-10-12 09:03:58 -0400

    Hello world and I am rich projects.

---
+++ Hello_World/Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Curtis Stilwell on 1/20/19.
+//  Copyright © 2019 Curtis Stilwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello")
+    }
+}
+
