b11ce3b0cb6d981e320e794696abbc2b94facc1b
Curtis-Stilwell <stilcurt@yahoo.com>
2020-02-15 07:39:28 -0500

    Created a profile card using Swift UI

---
+++ AngelaCard/AngelaCard/InfoView.swift
@@ -0,0 +1,37 @@
+//
+//  InfoView.swift
+//  AngelaCard
+//
+//  Created by Curtis Stilwell on 2/15/20.
+//  Copyright © 2020 Curtis Stilwell. All rights reserved.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text).foregroundColor(.black)
+                
+            })
+            
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
