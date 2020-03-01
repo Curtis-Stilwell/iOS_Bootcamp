7511f6c0a823af8cd519c2adaeb5224901d01e08
Curtis-Stilwell <stilcurt@yahoo.com>
2020-03-01 16:36:16 -0500

    Created a music playlist app by following a raywenderlich tutorial.

---
+++ RWBlueLibrary-Part1-Starter/RWBlueLibrary/NotificationExtension.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationExtension.swift
+//  RWBlueLibrary
+//
+//  Created by Curtis Stilwell on 3/1/20.
+//  Copyright © 2020 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+  static let BLDownloadImage = Notification.Name("BLDownloadImageNotification")
+}
